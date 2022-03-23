5769c798d3cba5ad5acc8f8c0f114526932088dd
aenuen <aenuen@qq.com>
2022-03-23 23:44:20 +0800

    安装pretty-quick

--- src/api/user.ts
+++ src/api/user.ts
@@ -1,6 +1,12 @@
 import axios from '@/libs/axios';
 
-export interface UserLoginFace {username: string;password: string;authCode?:string;sid?:string}
-export const userLogin = (loginData: UserLoginFace) => axios.post('/user/login', loginData)
+export interface UserLoginFace {
+  username: string;
+  password: string;
+  authCode?: string;
+  sid?: string;
+}
+export const userLogin = (loginData: UserLoginFace) =>
+  axios.post('/user/login', loginData);
 
-export const userInfo = () => axios.get('/user/info')
+export const userInfo = () => axios.get('/user/info');
