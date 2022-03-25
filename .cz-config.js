ace675f6fb0b75401c2c6e363095b4e886857630
aenuen <aenuen@qq.com>
2022-03-25 16:12:44 +0800

    refactor(deps):

--- .cz-config.js
+++ .cz-config.js
@@ -1,6 +1,6 @@
 module.exports = {
-  // type 类型（定义之后，可通过上下键选择）
   types: [
+    // type 类型（定义之后，可通过上下键选择）
     { value: 'feat', name: 'feat:     新增功能' },
     { value: 'fix', name: 'fix:      修复 bug' },
     { value: 'docs', name: 'docs:     文档变更' },
@@ -25,9 +25,8 @@ module.exports = {
     },
     { value: 'revert', name: 'revert:   回滚 commit' }
   ],
-
-  // scope 类型（定义之后，可通过上下键选择）
   scopes: [
+    // scope 类型（定义之后，可通过上下键选择）
     ['components', '组件相关'],
     ['hooks', 'hook 相关'],
     ['utils', 'utils 相关'],
@@ -36,40 +35,14 @@ module.exports = {
     ['deps', '项目依赖'],
     ['auth', '对 auth 修改'],
     ['other', '其他修改'],
-    // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
-    ['custom', '以上都不是？我要自定义']
+    ['custom', '以上都不是？我要自定义'] // 如果选择 custom，后面会让你再输入一个自定义的 scope。也可以不设置此项，把后面的 allowCustomScopes 设置为 true
   ].map(([value, description]) => {
-    return {
-      value,
-      name: `${value.padEnd(30)} (${description})`
-    };
+    return { value, name: `${value.padEnd(30)} (${description})` }
   }),
-
-  // 是否允许自定义填写 scope，在 scope 选择的时候，会有 empty 和 custom 可以选择。
-  // allowCustomScopes: true,
-
-  // allowTicketNumber: false,
-  // isTicketNumberRequired: false,
-  // ticketNumberPrefix: 'TICKET-',
-  // ticketNumberRegExp: '\\d{1,5}',
-
-  // 针对每一个 type 去定义对应的 scopes，例如 fix
-  /*
-  scopeOverrides: {
-    fix: [
-      { name: 'merge' },
-      { name: 'style' },
-      { name: 'e2eTest' },
-      { name: 'unitTest' }
-    ]
-  },
-  */
-
-  // 交互提示信息
   messages: {
+    // 交互提示信息
     type: '确保本次提交遵循 Angular 规范！\n选择你要提交的类型：',
     scope: '\n选择一个 scope（可选）：',
-    // 选择 scope: custom 时会出下面的提示
     customScope: '请输入自定义的 scope：',
     subject: '填写简短精炼的变更描述：\n',
     body: '填写更加详细的变更描述（可选）。使用 "|" 换行：\n',
@@ -77,15 +50,7 @@ module.exports = {
     footer: '列举出所有变更的 ISSUES CLOSED（可选）。 例如: #31, #34：\n',
     confirmCommit: '确认提交？'
   },
-
-  // 设置只有 type 选择了 feat 或 fix，才询问 breaking message
   allowBreakingChanges: ['feat', 'fix'],
-
-  // 跳过要询问的步骤
-  // skipQuestions: ['body', 'footer'],
-
-  subjectLimit: 100, // subject 限制长度
-  breaklineChar: '|' // 换行符，支持 body 和 footer
-  // footerPrefix : 'ISSUES CLOSED:'
-  // askForBreakingChangeFirst : true,
-};
+  subjectLimit: 100,
+  breaklineChar: '|'
+}
